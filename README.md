e2f7b96ffb3e3780e28e577ecdfa3486e5d90d4d
Regina <paulodebatin@gmail.com>
2020-11-08 18:25:50 -0300

    Inicio tela de pesquisa

---
+++ README.md
@@ -0,0 +1,18 @@
+#CRUD
+
+O objetivo deste projeto é servir de base para um estudo básico de como construir um cadastro (CRUD) usando algumas tecnologias no no Front-End e no Back-End.
+Tecnologias usadas:
+
+##Front-End
+Vue.js - pasta: frontend-vuejs
+React (a ser desenvolvido)
+Angular (a ser desenvolvido)
+
+##Back-end:
+Json Server - pasta: backend-jsonserver
+Node.js (a ser desenvolvido)
+Java Spring Boot (a ser desenvolvido)
+C# (a ser desenvolvido)
+Python (a ser desenvolvido)
+
+Para startar o front-end e o back-end entrar em cada pasta e verificar instruções.
---
+++ frontend-vuejs/src/components/pessoa/PessoaSel.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="frmPessoaSel">
+    <form>
+      <PesquisaComponent :entidade="entidade" @atualizacao-entidade="atualizaEntidade"/>
+
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in entidade" v-bind:key="item.id">
+            <td><a :href="'pessoasman/' + item.id">{{ item.id }}</a></td>
+            <td>{{ item.nome }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  </div>
+</template>
+
+<script>
+import PesquisaComponent from '../PesquisaComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PesquisaComponent
+  },
+
+  data() {
+    return {
+      entidade: []
+         
+    }  
+  },
+
+  methods: {
+    atualizaEntidade (dados) {
+      this.entidade = dados 
+    }
+  }  
+}
+</script>
+
+<style>
+
+</style>
