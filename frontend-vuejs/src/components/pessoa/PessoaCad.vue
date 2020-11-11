c26bc1f65b75a3e3f943fc66c789e1db46013ef7
Regina <paulodebatin@gmail.com>
2020-11-10 21:21:23 -0300

    Novos campos na tela de cadastro e pesquisa

--- frontend-vuejs/src/components/pessoa/PessoaCad.vue
+++ frontend-vuejs/src/components/pessoa/PessoaCad.vue
@@ -25,39 +25,87 @@
             </div>
         </div>
 
-        <div class="form-group">
-          <label for="bairro">Bairro</label>
-          <input
-            class="form-control"
-            id="bairro"
-            v-model="entidade.bairro"
-            placeholder="Bairro"
-            name="bairro"
-          />
+        <div class="form-row">
+            <div class="form-group col-md-10">
+              <label for="bairro">Bairro</label>
+              <input
+                class="form-control"
+                id="bairro"
+                v-model="entidade.bairro"
+                placeholder="Bairro"
+                name="bairro"
+              />
+            </div>
+            <div class="form-group col-md-2">
+              <label for="cep">Cep</label>
+              <input
+                class="form-control"
+                id="cep"
+                v-model="entidade.cep"
+                placeholder="Cep"
+                name="cep"
+              />
+            </div>
         </div>
 
-        <div class="form-group">
-          <label for="cidade">Cidade</label>
-          <input
-            class="form-control"
-            id="cidade"
-            v-model="entidade.cidade"
-            placeholder="Cidade"
-            name="cidade"
-          />
+        <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="cidade">Cidade</label>
+              <select id="cidade" class="form-control"  v-model="entidade.cidade">
+                <option value="1">Blumenau</option>
+                <option value="2">Gaspar</option>
+                <option value="3">Indaial</option>
+                <option value="4">Brusque</option>
+              </select>
+            </div>
+
+            <div class="form-group col-md-6">
+              <label for="uf">Uf</label>
+              <select id="uf" class="form-control"  v-model="entidade.uf">
+                <option value="1">Santa Catarina</option>
+                <option value="2">Paraná</option>
+                <option value="3">Rio Grande do Sul</option>
+              </select>
+            </div>
+        </div>   
+
+        <div class="form-row">
+            <div class="form-group col-md-2">
+              <label for="dataNascimento">Data de nascimento</label>
+              <input
+                class="form-control"
+                id="dataNascimento"
+                v-model="entidade.dataNascimento"
+                placeholder="Data de nascimento"
+                name="dataNascimento"
+              />
+            </div>
         </div>
 
 
-        <div class="form-group">
-          <label for="uf">Uf</label>
-          <input
-            class="form-control"
-            id="uf"
-            v-model="entidade.uf"
-            placeholder="Uf"
-            name="uf"
-          />
-        </div>
+        <div class="form-row">
+            <div class="form-group col-md-2">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="receberNoticias" v-model="entidade.receberNoticias">
+                <label class="form-check-label" for="receberNoticias">
+                  Receber notícias
+                </label>
+              </div>
+            </div>
+
+            <div class="form-group col-md-6">
+              <label for="uf">Fumante:</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="fumante" id="fumanteSim" value="S" v-model="entidade.fumante"> 
+                <label class="form-check-label" for="fumanteSim">Sim</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="fumante" id="fumanteNao" value="N" v-model="entidade.fumante">
+                <label class="form-check-label" for="fumanteNao">Não</label>
+              </div>
+
+            </div>
+        </div>   
 
 
       </div>
--- frontend-vuejs/src/components/pessoa/PessoaSel.vue
+++ frontend-vuejs/src/components/pessoa/PessoaSel.vue
@@ -8,12 +8,14 @@
           <tr>
             <th scope="col">Id</th>
             <th scope="col">Nome</th>
+            <th scope="col">Endereço</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="item in entidade" v-bind:key="item.id">
             <td><a :href="'pessoascad/' + item.id">{{ item.id }}</a></td>
             <td>{{ item.nome }}</td>
+            <td>{{ item.endereco}} - {{ item.numero }} - {{ item.bairro }}</td>
           </tr>
         </tbody>
       </table>
