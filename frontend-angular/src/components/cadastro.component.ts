47e65d2ba2f407cd7f189e4a6e761314d041aa45
Regina <paulodebatin@gmail.com>
2020-11-23 21:38:49 -0300

    inciando projeto angular

---
+++ frontend-angular/src/components/cadastro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cadastro-component',
+  templateUrl: './cadastro.component.html',
+  //styleUrls: ['./app.component.css']
+})
+export class CadastroComponent {
+}
---
+++ frontend-angular/src/components/pesquisa.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pesquisa-component',
+  templateUrl: './pesquisa.component.html',
+  //styleUrls: ['./app.component.css']
+})
+export class PesquisaComponentBase {
+}
